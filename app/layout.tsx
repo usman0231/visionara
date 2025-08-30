83892ac36d991a269b51cb8b984ae3af145e863a
usman <44198085+usman0231@users.noreply.github.com>
2025-08-30 04:26:40 -0700

    script update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./css/globals.css";
 import "./css/navbar.css";
 import "./css/bg.css";
 import Nav from 'nav';
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "VISIONARA",
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script src="https://cdn.lordicon.com/lordicon.js"></script>
+        <Script src="https://cdn.lordicon.com/lordicon.js"></Script>
       </head>
       <body>
         {children}
