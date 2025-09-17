c47cc71e409284534da66c062ebf5166c2e5fc28
virus0231 <usman@youronlineconversation.com>
2025-09-17 16:56:05 +0500

    an

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
 
   return (
       <html lang="en">
-        <Analytics/>
         <body className="overflow-x-hidden">
           {children}
           <Nav/>
+          <Analytics/>
         </body>
       </html>
   );
