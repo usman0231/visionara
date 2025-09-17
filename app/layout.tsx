16793c26b7c7dbf6a134d943287b08d26c8c8926
virus0231 <usman@youronlineconversation.com>
2025-09-17 17:11:18 +0500

    new meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,32 @@ export const metadata: Metadata = {
   title: "VISIONARA",
   description: "Visionara, turn your visions into a reality.",
   icons: {
-    icon: "/images/fav.png",
+    icon: "/images/fav.png", // Your favicon
+  },
+  openGraph: {
+    title: "VISIONARA",
+    description: "Turn your visions into reality with our innovative solutions.",
+    url: "https://www.visionara.ca", // Your site URL
+    siteName: "VISIONARA",
+    images: [
+      {
+        url: "/images/just_logo.png", // Public folder image
+        width: 1200,
+        height: 630,
+        alt: "VISIONARA - Your Vision, Our Technology",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "VISIONARA",
+    description: "Turn your visions into reality with our innovative solutions.",
+    images: ["/images/just_logo.png"], // Same image for Twitter preview
   },
 };
+
 
 export default function RootLayout({
   children,
